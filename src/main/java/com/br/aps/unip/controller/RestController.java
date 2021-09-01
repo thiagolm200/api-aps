4e6b0db7136641624e364ac343de7b693b2724e0
thiagolm200 <thiaguinholm200@hotmail.com>
2021-08-31 22:03:48 -0300

    Ajuste response

--- src/main/java/com/br/aps/unip/controller/RestController.java
+++ src/main/java/com/br/aps/unip/controller/RestController.java
@@ -1,5 +1,7 @@
 package com.br.aps.unip.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
@@ -13,13 +15,13 @@ public class RestController {
 
 	
 	@GetMapping(value="info")
-	private ResponseInfo getInformacoes() {
+	private ResponseEntity<ResponseInfo> getInformacoes() {
 		
 		RestTemplate restTemplate = new RestTemplate();
 		RequestInfo request = restTemplate.getForObject("http://api.weatherapi.com/v1/current.json?key=b3a3e43a20334c3f8a1235239212408&q=Sao Paulo&aqi=yes&lang=pt", RequestInfo.class);
 		
 		ResponseInfo response = new ResponseInfo(request);
 		
-		return response;
+		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
 }
